f2025c607593b871bb697aca7889abbedc5f2aeb
nilgunnb <nilgunnb@code.edu.az>
2023-04-23 17:17:36 +0400

    uploaded

---
+++ Delegate/Program.cs
@@ -0,0 +1,28 @@
+﻿class CustomList<T> where T : class
+{
+    private T[] List;
+
+    public CustomList()
+    {
+        List = new T[0];
+    }
+
+    public void Add(T item)
+    {
+        Array.Resize (ref List, List.Length+1);
+
+        List[List.Length-1] = item;
+    }
+
+    public T Find(Predicate<T> func)
+    {
+        foreach (T item in List)
+        {
+            if (func(item))
+            {
+                return item;
+            }
+        }
+        return null;
+    }
+}
